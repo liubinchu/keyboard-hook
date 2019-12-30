f54de883e1e82710908a3e0e8b679082c69491fd
liubi <liubinchu@outlook.com>
2019-12-30 22:14:05 +0300

    comments

--- keyboardHook/keyboardHook.cpp
+++ keyboardHook/keyboardHook.cpp
@@ -18,14 +18,14 @@ LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPTSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPTSTR    lpCmdLine,
+	_In_ int       nCmdShow)
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
- 	// TODO: Place code here.
+	// TODO: Place code here.
 	MSG msg;
 	char text[] = "Error loading DLL!";
 	char title[] = "Key Tracer";
@@ -33,13 +33,18 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	HINSTANCE dllhinst;
 	typedef VOID(CALLBACK* LPFNDLLFUNC1)(VOID);
 	LPFNDLLFUNC1 lpfnDllFunc1;
-
+	//https://docs.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-loadlibrarya
+	//Loads the specified module into the address space of the calling process. The specified module may cause other modules to be loaded.
 	dllhinst = LoadLibrary("keyHook.dll");
 	if (dllhinst != NULL)
 	{
+		//https://docs.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-getprocaddress
+		//Retrieves the address of an exported function or variable from the specified dynamic-link library (DLL).
 		lpfnDllFunc1 = (LPFNDLLFUNC1)GetProcAddress(dllhinst, "SetKbHook");
 		if (!lpfnDllFunc1)
 		{
+			//Frees the loaded dynamic-link library (DLL) module and, if necessary, decrements its reference count.
+			//https://docs.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-freelibrary
 			FreeLibrary(dllhinst);
 			error = TRUE;
 		}
@@ -51,10 +56,14 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	else error = TRUE;
 
 	if (error)
+		//https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getdesktopwindow
+		//Retrieves a handle to the desktop window.
 		MessageBox(GetDesktopWindow(), text, title, MB_OK);
 
 	while (GetMessage(&msg, 0, 0, 0))
 	{
+		//https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-translatemessage
+		//Translates virtual-key messages into character messages. 
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	};
